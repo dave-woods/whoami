b567709e2d72e6cafb2f56370f412b4c28d9722e
Dave Woods <dwoodscs@gmail.com>
2016-07-30 03:38:39 +0000

    Application logic

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const useragent = require('express-useragent');
 const port = process.env.PORT || 8080;
+
+app.use(useragent.express());
 
 app.get('/', function(req, res) {
 	const resObj = {ipaddress: null, language: null, software: null};
-
+    
+    resObj.ipaddress = req.headers['x-forwarded-for'];
+    resObj.language = req.headers['accept-language'].split(',')[0];
+    resObj.software = req.useragent.browser + ', ' + req.useragent.os;
+    
 
 	res.json(resObj);
 });
